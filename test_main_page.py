16d00ddc1fff6981c5aa3e1b33dca26203daddf4
zayaz <zayaz2805@gmail.com>
2022-08-01 21:18:58 +0200

    minor changes

--- test_main_page.py
+++ test_main_page.py
@@ -1,18 +1,20 @@
 from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
-main_link = "http://selenium1py.pythonanywhere.com/"
 login_link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+link = "http://selenium1py.pythonanywhere.com"
 
 
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, main_link)
+    page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, main_link)
+    page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
