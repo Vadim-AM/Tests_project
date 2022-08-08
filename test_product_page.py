5533fc5a1bf74bfbad122deab522411f4b80726d
zayaz <zayaz2805@gmail.com>
2022-08-08 13:17:39 +0200

    Parametrize and xmarks

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,15 @@
+import pytest
+
 from .pages.product_page import ProductPage
 
-product_page_link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear)'
+
+# product_page_link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
 
 
-def test_guest_can_add_product_to_basket(browser):
-    page = ProductPage(browser, product_page_link)
+@pytest.mark.parametrize('num', [*range(1, 7), pytest.param(7, marks=pytest.mark.xfail), *range(8, 10)])
+def test_guest_can_add_product_to_basket(browser, num):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num}"
+    page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
